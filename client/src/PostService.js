cf5b15b10339362b2d00d378a2d8a404a57ed729
TaeHwanL <soslth@naver.com>
2022-02-04 10:09:17 +0900

    4444

--- client/src/PostService.js
+++ client/src/PostService.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 class PostService {
-    static getPosts(currentPageIndex) {
+    static getPosts(state, currentPageIndex, complete, searchtxt) {
         return new Promise((resolve, reject) => {
             try {
                 let data = '';
-                axios.get('http://localhost:5000/search/' + currentPageIndex).then(function(response) {
+                axios.get('http://localhost:5000/search?state=' + state + '&page=' + currentPageIndex + '&complete=' + complete + '&searchtxt=' + searchtxt).then(function(response) {
                     data = response.data;
                     
                     resolve(
@@ -24,30 +24,24 @@ class PostService {
         });
     }
 
-    // static getPosts() {
-    //     let data = '';
-    //     try {
-    //         let result;
-
-    //         axios.get(url).then(function(response) {
-    //             data = response.data;
-    //             console.log(data + '11')
-    //             result = data.map(post => ({
-    //                 ...post,
-    //                 apply_start_time: new Date(post.apply_start_time),
-    //                 apply_end_time: new Date(post.apply_end_time),
-    //                 register_time: new Date(post.register_time),
-    //                 is_complete: post.is_complete === true ? "O" : ""
-    //             }))
-
-    //             return result;
-    //         });           
-    //         console.log(result + '1')
-    //         return result;
-    //     } catch (err) {
-    //         return err
-    //     }
-    // }
+    static gettotal(state, complete, searchtxt) {
+        return new Promise((resolve, reject) => {
+            try {
+                let data = '';
+                axios.get('http://localhost:5000/total?state=' + state + '&complete=' + complete + '&searchtxt=' + searchtxt).then(function(response) {
+                    data = response.data;
+                    
+                    resolve(
+                        data.map(post => ({
+                            ...post
+                        }))
+                    );
+                });
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
     
     static getPostCnt() {        
         return new Promise((resolve, reject) => {
---
+++ client/src/login.vue
@@ -0,0 +1,61 @@
+<template>
+<div id="app">
+  <!-- <router-link to="App">dd</router-link> -->
+  <!-- <router-view></router-view> -->
+    <div class="mb-3">
+      <label for="memid" class="form-label">아이디</label>
+      <input type="email" class="form-control" id="memid" placeholder="아이디">
+    </div>
+    <div class="mb-3">
+      <label for="mempw" class="form-label">비밀번호</label>
+      <input type="password" class="form-control" id="mempw" placeholder="비밀번호">
+    </div>
+    <div class="mb-3">
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="dropdownCheck">
+        <label class="form-check-label" for="dropdownCheck">
+          아이디 저장
+        </label>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary" @click="test">로그인</button>
+</div>
+</template>
+
+<script>
+// import { mapState, mapActions } from "vuex"
+import axios from 'axios';
+
+export default {
+    name: "app",
+    data() {
+      return {
+        id: null,
+        password: null
+      }
+    },
+    // computed: {
+    //   ...mapState(["isLogin", "isLoginError"])
+    // },
+    methods: {
+      // ...mapActions(["login"])
+      test() {
+        axios
+          .get("https://reqres.in/api/users?page=2")
+          .then(res => {
+            console.log(res)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+          .then(() => {
+
+          })
+      }
+    }
+}
+</script>
+
+<style>
+
+</style>
